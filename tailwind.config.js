544df36124eaf29f16cc7dfba7b2919553af9af5
HenriqueSSan <hsstudentwork@gmail.com>
2023-02-22 03:03:38 -0300

    refactor: remove folders and files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,40 @@
 module.exports = {
   content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
+    screens: {
+      sm: '0px',
+      ns: '769px',
+      md: '1025px',
+      lg: '1440px',
+      xl: '1921px',
+      xxl: '2500px',
+    },
+
+    colors: {
+      common: {
+        white: {
+          DEFAULT: '#FFFFFF',
+          200: '#EFF8FF',
+          400: '#EFF8FF',
+        },
+      },
+
+      blue: {
+        DEFAULT: '#0F9AFE',
+      },
+
+      gray: {
+        DEFAULT: '#6E7275',
+        200: '#293145',
+        light: '#DBDBDB',
+      },
+    },
+
+    fontFamily: {
+      primary: ['Inter', 'Helvetica', 'Arial', 'sans-serif'],
+      secondary: ['Inter', 'Helvetica', 'Arial', 'sans-serif'],
+    },
+
     extend: {},
   },
   plugins: [],
